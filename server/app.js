8b8ac2a64e3c8d5d3841e709460086d7e4b0f8b2
Rob Moore <rmoore.pageonepower@gmail.com>
2016-05-05 19:21:25 -0600

    able to send data to the database

--- server/app.js
+++ server/app.js
@@ -1,10 +1,27 @@
 import express from 'express'
+import db from 'sequelize-connect'
+import path from 'path'
+import pollController from './controllers/pollController'
+import bodyParser from 'body-parser'
 
-const app = express()
+async function connect () {
+  db.discover = path.join(__dirname, 'models')
+  db.matcher = function shouldIMportModel (modelFileName) {
+    return true
+  }
+  await db.connect('addvote_schema', 'root', '')
+}
 
-app.get('*', function(req, res) {
-  res.send('hello')
-})
+(async function () {
+  try {
+    await connect()
+  } catch (e) {
+    console.log(`There was an error connecting: ${e}`)
+  }
+  const app = express()
+  app.use(bodyParser.json())
+  app.post('/api/poll', pollController.handlePost)
 
-const port = 8000
-app.listen(port, () => console.log('Running on port ${port}'))
+  const port = 3000
+  app.listen(port, () => console.log(`Running on port ${port}`))
+})()
